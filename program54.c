3bacee4b272a4c950774841219fd8e2b1fb23822
Ankur Takale <takaleankur@gmail.com>
2026-01-06 00:07:34 +0530

    Initial commit

---
+++ program54.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckPrime(int iNo)
+{
+    int iCnt = 0, iFrequency = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    for(iCnt = 2; iCnt <= (iNo / 2); iCnt++)
+    {
+        if((iNo % iCnt) == 0)
+        {
+            iFrequency++;
+        }
+    }
+
+    if(iFrequency == 0)             //No Factors
+    {
+        return true;
+    }
+    else                            //At least one factor
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    int iValue = 0, bRet = false;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    bRet = CheckPrime(iValue);
+
+    if(bRet == true)
+    {
+        printf("%d is a prime number",iValue);
+    }
+    else
+    {
+        printf("%d is not a prime number",iValue);
+    }
+
+    return 0;
+}
---
+++ program56.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckPrime(int iNo)
+{
+    int iCnt = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    for(iCnt = 2; iCnt <= (iNo / 2); iCnt++)
+    {
+        if((iNo % iCnt) == 0)
+        {
+            break;                  //Optimization
+        }
+    }
+
+    if(iCnt > (iNo / 2))             //No Factors
+    {
+        return true;
+    }
+    else                            //At least one factor
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    int iValue = 0, bRet = false;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    bRet = CheckPrime(iValue);
+
+    if(bRet == true)
+    {
+        printf("%d is a prime number",iValue);
+    }
+    else
+    {
+        printf("%d is not a prime number",iValue);
+    }
+
+    return 0;
+}
+
+/*
+    Time Complexity : For prime - N/2
+    Time Complexity : For non prime - either 1(for even) or 2(for odd)
+*/
