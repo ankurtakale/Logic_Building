e7184a36a0612fad7059cd2b033cf189d5788584
Ankur Takale <takaleankur@gmail.com>
2026-03-01 00:38:47 +0530

    Initial commit

---
+++ program385.js
@@ -0,0 +1,16 @@
+function main()
+{
+    let str = "Hello";
+    console.log(typeof(str));
+    console.log(str.charAt(0));
+    console.log(str.charAt(1));
+    console.log(str.charAt(2));
+
+    let Arr = str.split("");
+    console.log(typeof(Arr));
+    console.log(Arr[0]);
+    console.log(Arr[1]);
+    console.log(Arr[2]);
+}
+
+main();
