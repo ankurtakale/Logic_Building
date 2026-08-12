aacb45b9e01bc6a39231c1b9c8f79a508577a99a
Ankur Takale <takaleankur@gmail.com>
2026-08-12 22:10:48 +0530

    LB

---
+++ program708.cpp
@@ -0,0 +1,34 @@
+// Input    :   7891
+// Output   :   7+8+9+1 = 25
+
+#include<iostream>
+using namespace std;
+
+int SumDigits(int iNo)
+{
+    int iDigit = 0;
+    static int iSum  = 0;
+
+    if(iNo != 0)
+    {
+        iDigit = iNo % 10;
+        iSum = iSum + iDigit;
+        SumDigits(iNo / 10);
+    }
+
+    return iSum;
+}
+
+int main()
+{
+    int iValue = 0, iRet = 0;
+
+    cout<<"Enter number : \n";
+    cin>>iValue;
+
+    iRet = SumDigits(iValue);
+
+    cout<<"Sum of digits is : "<<iRet<<"\n";
+
+    return 0;
+}
---
+++ program720.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int Countapital(char *str)
+{
+    static int iCount = 0;
+
+    if(*str != '\0')
+    {
+        if((*str >= 'A') && (*str <= 'Z'))
+        {
+            iCount++;
+        }
+        str++;
+        Countapital(str);
+    }
+
+    return iCount;
+}
+
+int main()
+{
+    char Arr[50] = {'\0'};
+    int iRet = 0;
+
+    printf("Enter string : \n");
+    scanf("%[^\n]s",Arr);
+
+    iRet = Countapital(Arr);
+
+    printf("Capital count is : %d\n",iRet);
+
+    return 0;
+}
