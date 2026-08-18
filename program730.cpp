c0ddc432645a471b57674e311167d961d72d68f9
Ankur Takale <takaleankur@gmail.com>
2026-08-18 23:10:44 +0530

    LB

---
+++ program730.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+void strRevDisplay(char *str)
+{
+    if(*str != '\0')
+    {
+        cout<<str<<"\n";
+        str++;
+        strRevDisplay(str);
+    }
+}
+
+int main()
+{
+    char Arr[50] = {'\0'};
+
+    printf("Enter string : \n");
+    scanf("%[^\n]s",Arr);
+
+    strRevDisplay(Arr);
+
+    return 0;
+}
